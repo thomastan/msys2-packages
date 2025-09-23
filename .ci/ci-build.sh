b63e4a55f9997c2c5583d2e107643c71bf35bb7e
Thomas Tan <thomas.tan@msn.com>
2025-09-23 13:04:13 -0700

    Also redirect stderr to log artifact

--- .ci/ci-build.sh
+++ .ci/ci-build.sh
@@ -113,18 +113,18 @@ for package in "${packages[@]}"; do
         if [[ $pkgname == "ncurses" ]]; then
             message "Interrupting '$pkgname' meta-diff to fix gpgme"
             echo "***** Interuppting $pkgname meta-diff to fix GPGME" >> $PWD/../artifacts/$pkgname.log
-            cat /etc/pacman.conf >> $PWD/../artifacts/$pkgname.log
+            cat /etc/pacman.conf >> $PWD/../artifacts/$pkgname.log 2>&1
             message <<< "$(cat /etc/pacman.conf)"
             message "Install gnupug"
-            echo "Install GNUPG" >> $PWD/../artifacts/$pkgname.log
-            pacman -S --noconfirm gnupg >> $PWD/../artifacts/$pkgname.log
-            echo "***** which gpg" >> $PWD/../artifacts/$pkgname.log
-            which gpg >> $PWD/../artifacts/$pkgname.log
-            echo "***** gpg --version" >> $PWD/../artifacts/$pkgname.log
-            gpg --list-secret-keys >> $PWD/../artifacts/$pkgname.log
-            echo "***** Inspect /etc/pacman.conf" >> $PWD/../artifacts/$pkgname.log
-            cat /etc/pacman.conf >> $PWD/../artifacts/$pkgname.log
-            echo "***** gpg done" >> $PWD/../artifacts/$pkgname.log
+            echo "Install GNUPG" >> $PWD/../artifacts/$pkgname.log 2>&1
+            pacman -S --noconfirm gnupg >> $PWD/../artifacts/$pkgname.log 2>&1
+            echo "***** which gpg" >> $PWD/../artifacts/$pkgname.log 2>&1
+            which gpg >> $PWD/../artifacts/$pkgname.log 2>&1
+            echo "***** gpg --version" >> $PWD/../artifacts/$pkgname.log 2>&1
+            gpg --list-secret-keys >> $PWD/../artifacts/$pkgname.log 2>&1
+            echo "***** Inspect /etc/pacman.conf" >> $PWD/../artifacts/$pkgname.log 2>&1
+            cat /etc/pacman.conf >> $PWD/../artifacts/$pkgname.log 2>&1
+            echo "***** gpg done" >> $PWD/../artifacts/$pkgname.log 2>&1
         fi
 
         message "Package info diff for ${pkgname}"
