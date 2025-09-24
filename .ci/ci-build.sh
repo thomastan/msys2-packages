2086ca6113cd07b17dd1f4c48180f97ab6c17caa
Thomas Tan <thomas.tan@msn.com>
2025-09-24 10:12:59 -0700

    add a sleep to try wait

--- .ci/ci-build.sh
+++ .ci/ci-build.sh
@@ -129,6 +129,7 @@ for package in "${packages[@]}"; do
             # echo "which gpg2" >> $PWD/../artifacts/$pkgname.log 2>&1
             # which gpg >> $PWD/../artifacts/$pkgname.log 2>&1
             echo "***** gpg --version" >> $PWD/../artifacts/$pkgname.log 2>&1
+            sleep 5
             gpg --version >> $PWD/../artifacts/$pkgname.log 2>&1
             # gpg --list-secret-keys >> $PWD/../artifacts/$pkgname.log 2>&1
             # echo "***** Inspect /etc/pacman.conf" >> $PWD/../artifacts/$pkgname.log 2>&1
